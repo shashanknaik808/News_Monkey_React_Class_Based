a9c66a0cb2c383ecae65baea2e105d3722c7e1ab
Shashank Naik <shashanknaik808@gmail.com>
2023-12-01 15:44:47 +0530

    Fetched news data in componentDidMount using the News API and updated state with retrieved articles

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -10,6 +10,16 @@ export class News extends Component {
         }
     }
 
+    componentDidMount() {
+        fetch(
+            "https://newsapi.org/v2/top-headlines?country=us&apiKey=8788032716674e868d61331f7e5304c6"
+        )
+            .then(res => res.json())
+            .then(data => {
+                this.setState({ articles: data.articles });
+            });
+    }
+
     render() {
         return (
             <>
