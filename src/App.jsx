c9eb65eb32296ccbb86b76bf36fb331fc3d181e8
Shashank Naik <shashanknaik808@gmail.com>
2023-12-01 12:01:07 +0530

    Imported and used Navbar component in the main application

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 
 export class App extends Component {
   render() {
     return (
-      <div>Hello World</div>
+      <Navbar />
     )
   }
 }
