3ad89c84e5f9ed5d1d1cde1f0e15adb5a2d5b92f
Shashank Naik <shashanknaik808@gmail.com>
2023-12-01 16:13:56 +0530

    Added 'rel=noreferrer' to anchor tag in NewsItem.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ export class Navbar extends Component {
                                 <a className="nav-link active" aria-current="page" href="/">Home</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/about">About</a>
+                                <a rel="noreferrer" className="nav-link" href="/about">About</a>
                             </li>
                         </ul>
                     </div>
