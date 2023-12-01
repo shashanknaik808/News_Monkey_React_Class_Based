60dfdb73d722b226388fe6c95dcaa3d26437d14c
Shashank Naik <shashanknaik808@gmail.com>
2023-12-01 12:43:59 +0530

    Updated News component: added a container, heading, and specific titles/descriptions to NewsItems.

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -5,12 +5,20 @@ export class News extends Component {
     render() {
         return (
             <>
-                <div>News</div>
-                <NewsItem />
-                <NewsItem />
-                <NewsItem />
-                <NewsItem />
-                <NewsItem />
+                <div className='container my-3'>
+                    <h2>NewsMonkey - Top Headlines</h2>
+                    <div className='row'>
+                        <div className='col-md-4'>
+                            <NewsItem title="my title" description="abcd" />
+                        </div>
+                        <div className='col-md-4'>
+                            <NewsItem title="my title" description="abcd" />
+                        </div>
+                        <div className='col-md-4'>
+                            <NewsItem title="my title" description="abcd" />
+                        </div>
+                    </div>
+                </div>
             </>
         )
     }
