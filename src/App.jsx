7c2b3640f50a72f4b494ead702984a08a1bc69b1
Shashank Naik <shashanknaik808@gmail.com>
2023-12-02 12:55:47 +0530

    Added unique keys to News components in route configuration

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,14 @@ export class App extends Component {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path='/' element={<News category='general' />} />
-          <Route path='/business' element={<News category='business' />} />
-          <Route path='/entertainment' element={<News category='entertainment' />} />
-          <Route path='/general' element={<News category='general' />} />
-          <Route path='/health' element={<News category='health' />} />
-          <Route path='/science' element={<News category='science' />} />
-          <Route path='/sports' element={<News category='sports' />} />
-          <Route path='/technology' element={<News category='technology' />} />
+          <Route path='/' element={<News key="general" category='general' />} />
+          <Route path='/business' element={<News key="business" category='business' />} />
+          <Route path='/entertainment' element={<News key="entertainment" category='entertainment' />} />
+          <Route path='/general' element={<News key="general" category='general' />} />
+          <Route path='/health' element={<News key="health" category='health' />} />
+          <Route path='/science' element={<News key="science" category='science' />} />
+          <Route path='/sports' element={<News key="sports" category='sports' />} />
+          <Route path='/technology' element={<News key="technology" category='technology' />} />
         </Routes>
       </BrowserRouter>
     )
