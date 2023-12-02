86bb6c9fe5b7814fa9b6622fe53d4c8c426f325e
Shashank Naik <shashanknaik808@gmail.com>
2023-12-02 12:43:19 +0530

    Set up routes for different news categories using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import News from './components/News';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export class App extends Component {
   render() {
     return (
-      <div>
+      <BrowserRouter>
         <Navbar />
-        <News category="health" />
-      </div>
+        <Routes>
+          <Route path='/' element={<News category='general' />} />
+          <Route path='/business' element={<News category='business' />} />
+          <Route path='/entertainment' element={<News category='entertainment' />} />
+          <Route path='/general' element={<News category='general' />} />
+          <Route path='/health' element={<News category='health' />} />
+          <Route path='/science' element={<News category='science' />} />
+          <Route path='/sports' element={<News category='sports' />} />
+          <Route path='/technology' element={<News category='technology' />} />
+        </Routes>
+      </BrowserRouter>
     )
   }
 }
