29cf8515a6bc6b476f13d7981847674c5dae2fe8
Shashank Naik <shashanknaik808@gmail.com>
2023-12-02 17:12:04 +0530

    Refactored fetchMoreData and handlePreviousClick for cleaner code and optimized API requests

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -49,75 +49,58 @@ export class News extends Component {
     }
 
     fetchMoreData = async () => {
-        if (this.state.page + 1 > Math.ceil(this.state.totalArticles / 10)) {
-            console.log("No more Articles");
-        }
-        else {
-            this.setState({ ...this.state, loading: true });
-            let res = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=8788032716674e868d61331f7e5304c6&page=${this.state.page + 1}&pageSize=10`);
-            let data = await res.json();
-            console.log(data.articles);
-            this.setState({
-                ...this.state,
-                page: this.state.page + 1,
-                articles: data.articles,
-                totalArticles: data.totalResults,
-                loading: false
-            })
-        }
-    }
-
-    async handlePreviousClick() {
-        this.setState({ ...this.state, loading: true });
-        let res = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=8788032716674e868d61331f7e5304c6&page=${this.state.page + 1}&pageSize=10`);
+        let res = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=bd002a1cf4f64969a8819fcbfe3a190f&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`);
         let data = await res.json();
         this.setState({
-            ...this.state,
-            page: this.state.page - 1,
-            articles: data.articles,
-            totalArticles: data.totalResults,
-            loading: false
-        })
+            articles: this.state.articles.concat(data.articles),
+            loading: false,
+            page: this.state.page + 1,
+            totalArticles: data.totalResults
+        });
     }
 
     render() {
 
-        console.log(this.state.articles);
-
         return (
             <>
                 <div className='container my-3'>
-                    <h2 className='text-center' >NewsMonkey - Top Headlines</h2>
+                    <h2 className='text-center' style={{ margin: '90px 0px 40px 0px' }}>News Monkey - Top Headlines</h2>
                     {this.state.loading && <Spinner />}
-                    {(this.state.loading !== true) &&
-                        <div className="row">
-                            {
-                                this.state.articles.map((item, index) => {
-                                    return (
-                                        <div className="col-md-4" key={index}>
-                                            <NewsItem
-                                                title={item.title.slice(0, 45)}
-                                                decription={(item.description) ? item.description.slice(0, 88) : "Click for more Details"}
-                                                imageUrl={(item.urlToImage != null) ? item.urlToImage : "https://theleaflet.in/wp-content/uploads/2021/09/IT-Dept.jpg"}
-                                                newsURL={item.url}
-                                                author={item.author}
-                                                date={item.publishedAt}
-                                                source={item.source.name}
-                                            />
-                                        </div>
-                                    )
-                                })
-                            }
-                        </div>
+                    {(!this.state.loading) &&
+                        <InfiniteScroll
+                            dataLength={this.state.articles.length}
+                            next={this.fetchMoreData}
+                            hasMore={this.state.articles.length != this.state.totalArticles}
+                            loader={<Spinner />}
+                        >
+                            <div className='container'>
+                                <div className="row">
+                                    {
+                                        this.state.articles.map((item, index) => {
+                                            return (
+                                                <div className="col-md-4" key={index}>
+                                                    <NewsItem
+                                                        title={item.title.slice(0, 45)}
+                                                        decription={(item.description) ? item.description.slice(0, 88) : "Click for more Details"}
+                                                        imageUrl={(item.urlToImage != null) ? item.urlToImage : "https://theleaflet.in/wp-content/uploads/2021/09/IT-Dept.jpg"}
+                                                        newsURL={item.url}
+                                                        author={item.author}
+                                                        date={item.publishedAt}
+                                                        source={item.source.name}
+                                                    />
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div>
+                        </InfiniteScroll>
                     }
-                    <div className='container d-flex justify-content-between'>
-                        <button disabled={this.state.page <= 1} type='button' className='btn btn-dark' onClick={this.handleNextClick}>&larr; Previous</button>
-                        <button disabled={this.state.page * 10 >= this.state.totalArticles} type='button' className='btn btn-dark' onClick={this.handlePreviousClick}>Next &rarr; </button>
-                    </div>
                 </div>
             </>
         )
     }
 }
+
 
 export default News;
