171d1310d8b58b77eb5d3316665ab5cdd62b045d
Shashank Naik <shashanknaik808@gmail.com>
2023-12-02 16:11:49 +0530

    Use local variables for country and pageSize in App component

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,22 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export class App extends Component {
   render() {
+
+    let vCon = 'in';
+    let vSiz = 10;
+
     return (
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path='/' element={<News key="general" category='general' />} />
-          <Route path='/business' element={<News key="business" category='business' />} />
-          <Route path='/entertainment' element={<News key="entertainment" category='entertainment' />} />
-          <Route path='/general' element={<News key="general" category='general' />} />
-          <Route path='/health' element={<News key="health" category='health' />} />
-          <Route path='/science' element={<News key="science" category='science' />} />
-          <Route path='/sports' element={<News key="sports" category='sports' />} />
-          <Route path='/technology' element={<News key="technology" category='technology' />} />
+          <Route path='/' element={<News key="general" country={vCon} pageSize={vSiz} />} />
+          <Route path='/business' element={<News key="business" country={vCon} pageSize={vSiz} category='business' />} />
+          <Route path='/entertainment' element={<News key="entertainment" country={vCon} pageSize={vSiz} category='entertainment' />} />
+          <Route path='/general' element={<News key="general" country={vCon} pageSize={vSiz} category='general' />} />
+          <Route path='/health' element={<News key="health" country={vCon} pageSize={vSiz} category='health' />} />
+          <Route path='/science' element={<News key="science" country={vCon} pageSize={vSiz} category='science' />} />
+          <Route path='/sports' element={<News key="sports" country={vCon} pageSize={vSiz} category='sports' />} />
+          <Route path='/technology' element={<News key="technology" country={vCon} pageSize={vSiz} category='technology' />} />
         </Routes>
       </BrowserRouter>
     )
