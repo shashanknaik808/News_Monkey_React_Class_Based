2ae034fa8c38a658183adb83b1949d27bae75710
Shashank Naik <shashanknaik808@gmail.com>
2023-12-02 17:17:56 +0530

    Refactored news rendering with InfiniteScroll for improved user experience

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -61,42 +61,47 @@ export class News extends Component {
     }
 
     render() {
-        console.log(this.state.articles);
+
         return (
-            <div className='container my-3'>
-                <h2 className='text-center' style={{ margin: '90px 0px 40px 0px' }}>News Monkey - Top Headlines</h2>
-                {this.state.loading && <Spinner />}
-                {(!this.state.loading) &&
-
-                    <div className="row">
-                        {
-                            this.state.articles.map((item, index) => {
-                                return (
-                                    <div className="col-md-4" key={index}>
-                                        <NewsItem
-                                            title={item.title.slice(0, 45)}
-                                            decription={(item.description) ? item.description.slice(0, 88) : "Click for more Details"}
-                                            imageUrl={(item.urlToImage != null) ? item.urlToImage : "https://theleaflet.in/wp-content/uploads/2021/09/IT-Dept.jpg"}
-                                            newsURL={item.url}
-                                            author={item.author}
-                                            date={item.publishedAt}
-                                            source={item.source.name}
-                                        />
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
-                }
-                <div className='container d-flex justify-content-between'>
-                    <button disabled={this.state.page <= 1} type='button' className='btn btn-dark' onClick={this.handlePreviousClick}>&larr; Privious</button>
-                    <button disabled={this.state.page * 10} type='button' className='btn btn-dark' onClick={this.handleNextClick}> Next &rarr; </button>
+            <>
+                <div className='container my-3'>
+                    <h2 className='text-center' style={{ margin: '90px 0px 40px 0px' }}>News Monkey - Top Headlines</h2>
+                    {this.state.loading && <Spinner />}
+                    {(!this.state.loading) &&
+                        <InfiniteScroll
+                            dataLength={this.state.articles.length}
+                            next={this.fetchMoreData}
+                            hasMore={this.state.articles.length != this.state.totalArticles}
+                            loader={<Spinner />}
+                        >
+                            <div className='container'>
+                                <div className="row">
+                                    {
+                                        this.state.articles.map((item, index) => {
+                                            return (
+                                                <div className="col-md-4" key={index}>
+                                                    <NewsItem
+                                                        title={item.title.slice(0, 45)}
+                                                        decription={(item.description) ? item.description.slice(0, 88) : "Click for more Details"}
+                                                        imageUrl={(item.urlToImage != null) ? item.urlToImage : "https://theleaflet.in/wp-content/uploads/2021/09/IT-Dept.jpg"}
+                                                        newsURL={item.url}
+                                                        author={item.author}
+                                                        date={item.publishedAt}
+                                                        source={item.source.name}
+                                                    />
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div>
+                        </InfiniteScroll>
+                    }
                 </div>
-            </div>
+            </>
+        )
     }
-        
-    }
-
+}
 
 
 export default News;
